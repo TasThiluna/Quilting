2b5a5d3b47004f8f7401f3b91c05bd9e10e0bdec
tasthing <lufarionite@gmail.com>
2021-10-02 03:57:28 -0400

    Progress

--- Assets/quilting.cs
+++ Assets/quilting.cs
@@ -14,15 +14,28 @@ public class quilting : MonoBehaviour
     public KMBombModule module;
 
     public KMSelectable[] buttons;
+    public KMSelectable scissors;
     public Color[] buttonColors;
+    public GameObject displayedPatch;
+    public GameObject lucky;
+    public Transform[] scissorBlades;
+
+    public Mesh[] puzzle0Meshes;
+    public Mesh[] puzzle1Meshes;
+    public Mesh[] puzzle2Meshes;
+    public Mesh[] puzzle3Meshes;
+    public Mesh[] puzzle4Meshes;
+    private Mesh[][] allMeshes = new Mesh[5][];
 
     private int puzzleIndex;
     private List<patch> patches = new List<patch>();
     private patch whitePatch;
     private List<int> displayedPatches = new List<int>();
-    private List<int> givenPatches = new List<int>();
     private QColor[] buttonOrder = new QColor[4];
     private QColor solution;
+
+    private int displayIndex;
+    private bool cycleStarted;
 
     private static int moduleIdCounter = 1;
     private int moduleId;
@@ -33,14 +46,20 @@ public class quilting : MonoBehaviour
         moduleId = moduleIdCounter++;
         foreach (KMSelectable button in buttons)
             button.OnInteract += delegate () { PressButton(button); return false; };
+        scissors.OnInteract += delegate () { PressScissors(); return false; };
     }
 
     private void Start()
     {
+        displayedPatch.SetActive(false);
+        lucky.SetActive(false);
+        allMeshes = new Mesh[][] { puzzle0Meshes, puzzle1Meshes, puzzle2Meshes, puzzle3Meshes, puzzle4Meshes };
+
         puzzleIndex = rnd.Range(0, 5);
-        buttonOrder = Enumerable.Range(0, 4).Select(x => (QColor) x).ToList().Shuffle().ToArray();
+        puzzleIndex = 0; //> TESTING
+        buttonOrder = Enumerable.Range(0, 4).Select(x => (QColor)x).ToList().Shuffle().ToArray();
         for (int i = 0; i < 4; i++)
-            buttons[i].GetComponent<Renderer>().material.color = buttonColors[(int) buttonOrder[i]];
+            buttons[i].GetComponent<Renderer>().material.color = buttonColors[(int)buttonOrder[i]];
         Debug.LogFormat("[Quilting #{0}] Using quilt {1}.", moduleId, puzzleIndex + 1);
         Debug.LogFormat("[Quilting #{0}] Button colors: {1}", moduleId, buttonOrder.Join(", "));
 
@@ -71,10 +90,10 @@ public class quilting : MonoBehaviour
         whitePatch = patches[whitePatchIx];
         solution = whitePatch.color;
         Debug.LogFormat("[Quilting #{0}] The color of the white patch is {1}.", moduleId, solution);
-        displayedPatches = givenPatches.ToList();
+        displayedPatches = givens.ToList();
         displayedPatches.Add(whitePatchIx);
-        // DISPLAY PATCHES COROUTINE
-        if (Application.isEditor)
+        displayedPatches.Shuffle();
+        if (Application.isEditor && false)
         {
             Debug.LogFormat("ID {0}", whitePatch.id);
             for (int i = 0; i < 20; i++)
@@ -112,7 +131,7 @@ public class quilting : MonoBehaviour
             yield break;
         }
 
-        shortcut:
+    shortcut:
         fewestPossibleColors.Shuffle();
         foreach (var color in fewestPossibleColors)
         {
@@ -125,6 +144,8 @@ public class quilting : MonoBehaviour
 
     private void PressButton(KMSelectable button)
     {
+        button.AddInteractionPunch(.25f);
+        audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonRelease, button.transform);
         if (moduleSolved)
             return;
         var ix = Array.IndexOf(buttons, button);
@@ -134,11 +155,65 @@ public class quilting : MonoBehaviour
             module.HandlePass();
             moduleSolved = true;
             Debug.LogFormat("[Quiltng #{0}] That was correct. Module solved!", moduleId);
+            scissors.Highlight.gameObject.SetActive(false);
+            if (!cycleStarted)
+                lucky.SetActive(true);
+            if (displayIndex == displayedPatches.IndexOf(whitePatch.id))
+                displayedPatch.GetComponent<Renderer>().material.color = buttonColors[(int)patches[whitePatch.id].color];
+            StartCoroutine(SolveAnimation(scissorBlades[0], 15f, true));
+            StartCoroutine(SolveAnimation(scissorBlades[1], -15f, false));
         }
         else
         {
             module.HandleStrike();
             Debug.LogFormat("[Quiltng #{0}] That was incorrect. Strike!", moduleId);
+        }
+    }
+
+    private void PressScissors()
+    {
+        if (moduleSolved)
+            return;
+        scissors.AddInteractionPunch(.25f);
+        audio.PlaySoundAtTransform("fabric" + rnd.Range(1, 6), displayedPatch.transform);
+        patch thisPatch;
+        if (cycleStarted)
+        {
+            displayIndex = (displayIndex + 1) % displayedPatches.Count;
+            var ix = displayedPatches[displayIndex];
+            thisPatch = patches[displayedPatches[displayIndex]];
+        }
+        else
+            thisPatch = patches[displayedPatches[0]];
+        displayedPatch.SetActive(true);
+        displayedPatch.GetComponent<MeshFilter>().mesh = allMeshes[puzzleIndex][thisPatch.id];
+        displayedPatch.GetComponent<Renderer>().material.color = thisPatch.id == whitePatch.id ? Color.white : buttonColors[(int)thisPatch.color];
+        cycleStarted = true;
+    }
+
+    private IEnumerator SolveAnimation(Transform blade, float angle, bool sound)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            var elapsed = 0f;
+            var duration = .25f;
+            if (sound)
+                audio.PlaySoundAtTransform("cut", scissors.transform);
+            while (elapsed < duration)
+            {
+                blade.localEulerAngles = new Vector3(0f, Easing.InOutSine(elapsed, 0f, angle, duration), 0f);
+                yield return null;
+                elapsed += Time.deltaTime;
+            }
+            blade.localEulerAngles = new Vector3(0f, angle, 0f);
+            elapsed = 0f;
+            while (elapsed < duration)
+            {
+                blade.localEulerAngles = new Vector3(0f, Easing.InOutSine(elapsed, angle, 0f, duration), 0f);
+                yield return null;
+                elapsed += Time.deltaTime;
+            }
+            blade.localEulerAngles = new Vector3(0f, 0f, 0f);
         }
     }
 
@@ -289,12 +364,39 @@ public class quilting : MonoBehaviour
 
     // Twitch Plays
 #pragma warning disable 414
-    private readonly string TwitchHelpMessage = "!{0} ";
+    private readonly string TwitchHelpMessage = "!{0} scissors [Presses the sewing scissors.] !{0} <red/yellow/blue/green> [Presses the round button of that color.]";
 #pragma warning restore 414
 
     IEnumerator ProcessTwitchCommand(string input)
     {
-        yield return null;
+        input = input.Trim().ToUpperInvariant();
+        if (input == "SCISSORS")
+        {
+            yield return null;
+            scissors.OnInteract();
+        }
+        else if (input == "RED")
+        {
+            yield return null;
+            buttons[Array.IndexOf(buttonOrder, QColor.red)].OnInteract();
+        }
+        else if (input == "YELLOW")
+        {
+            yield return null;
+            buttons[Array.IndexOf(buttonOrder, QColor.yellow)].OnInteract();
+        }
+        else if (input == "BLUE")
+        {
+            yield return null;
+            buttons[Array.IndexOf(buttonOrder, QColor.blue)].OnInteract();
+        }
+        else if (input == "GREEN")
+        {
+            yield return null;
+            buttons[Array.IndexOf(buttonOrder, QColor.green)].OnInteract();
+        }
+        else
+            yield break;
     }
 
     IEnumerator TwitchHandleForcedSolve()
